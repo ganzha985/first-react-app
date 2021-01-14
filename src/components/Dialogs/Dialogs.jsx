e29bb08365e2d9153140dfbaec26443f40fc71ba
Ганжа Дмитрий Евгеньевич <deganzha@FED.COM.UA>
2021-01-14 15:23:06 +0200

    Layout Dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,23 @@ import m from './Dialogs.module.css';
 
 const Dialogs = () => {
     return (
-        <div >
-            Dialogs
+        <div className={m.dialogs}>
+            <div className={m.dialog_items }>
+                <div className={m.dialog + " " + m.active}>
+                    Andrey
+                </div>
+                <div className={m.dialog}>
+                    Andrey
+                </div>
+                <div className={m.dialog}>
+                    Andrey
+                </div>   
+            </div>
+            <div className={m.messages}>
+                <div className={m.message}> mes 1</div>
+                <div className={m.message}> mes 2</div>
+                <div className={m.message}> mes 3</div>
+            </div>
         </div>
     )
 }
